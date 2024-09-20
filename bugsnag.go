cc20439bc9b693c82d9f70dad3e0f399d9a3b923
Michal Wasilewski <michal@mwasilewski.net>
2024-09-20 14:53:29 +0200

    remove unused type
    Signed-off-by: Michal Wasilewski <michal@mwasilewski.net>

--- bugsnag.go
+++ bugsnag.go
@@ -1,13 +1,5 @@
 package spcontext
 
-import (
-	"runtime"
-	"strings"
-
-	bugsnagerrors "github.com/bugsnag/bugsnag-go/v2/errors"
-	"github.com/pkg/errors"
-)
-
 // BugsnagLogger wraps the given Context inside a bugsnag friendly logger.
 type BugsnagLogger struct {
 	Ctx Context
@@ -16,62 +8,4 @@ type BugsnagLogger struct {
 // Printf logs the message with info level.
 func (l *BugsnagLogger) Printf(format string, v ...interface{}) {
 	l.Ctx.Infof(format, v...)
-}
-
-type stackTracer interface {
-	error
-	StackTrace() errors.StackTrace
-}
-
-// errorWithStackFrames satisfies bugsnag.ErrorWithStackFrames for a github.com/pkg/errors error.
-type errorWithStackFrames struct {
-	err stackTracer
-}
-
-// Cause returns initial error, provides compatibility for pkg/errors chains.
-func (w *errorWithStackFrames) Cause() error { return w.err }
-
-// Unwrap returns initial error, provides compatibility for Go 1.13 error chains.
-func (w *errorWithStackFrames) Unwrap() error { return w.err }
-
-func (e *errorWithStackFrames) Error() string {
-	return e.err.Error()
-}
-
-func (e *errorWithStackFrames) StackFrames() []bugsnagerrors.StackFrame {
-	stackTrace := e.err.StackTrace()
-
-	out := make([]bugsnagerrors.StackFrame, len(stackTrace))
-	for i, frame := range stackTrace {
-		pc := uintptr(frame) - 1
-		fn := runtime.FuncForPC(pc)
-		file, line := fn.FileLine(pc)
-
-		name := fn.Name()
-		var pkg string
-		var fnName string
-		if pkgDivider := strings.LastIndex(name, "/"); pkgDivider != -1 {
-			pkg = name[:pkgDivider]
-			fnName = name[pkgDivider+1:]
-			if fnDivider := strings.LastIndex(fnName, "."); fnDivider != -1 {
-				fnName = fnName[fnDivider+1:]
-			}
-		} else if pkgDivider := strings.Index(name, "."); pkgDivider != -1 {
-			pkg = name[:pkgDivider]
-			fnName = name[pkgDivider+1:]
-			if fnDivider := strings.LastIndex(fnName, "."); fnDivider != -1 {
-				fnName = fnName[fnDivider+1:]
-			}
-		}
-
-		out[i] = bugsnagerrors.StackFrame{
-			File:           file,
-			LineNumber:     line,
-			Name:           fnName,
-			Package:        pkg,
-			ProgramCounter: pc,
-		}
-	}
-
-	return out
 }
