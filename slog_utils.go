fbfc976a1b64394871e3cb291de2588976f37744
peterdeme <snypox@gmail.com>
2025-05-12 16:46:26 +0200

    feat: add NewJSONLoggerWithAtts function for enhanced JSON logging with attributes
    Signed-off-by: peterdeme <snypox@gmail.com>

--- slog_utils.go
+++ slog_utils.go
@@ -15,6 +15,12 @@ func NewTextLogger(w io.Writer) *slog.Logger {
 	return slog.New(slog.NewTextHandler(w, nil))
 }
 
+// NewJSONLoggerWithAtts creates a JSON slog logger that logs to the specified writer with the given attributes.
+func NewJSONLoggerWithAtts(w io.Writer, opts *slog.HandlerOptions, atts ...slog.Attr) *slog.Logger {
+	handler := slog.NewJSONHandler(w, opts)
+	return slog.New(handler.WithAttrs(atts))
+}
+
 // NewJSONLogger creates a JSON slog logger that logs to the specified writer.
 func NewJSONLogger(w io.Writer, opts *slog.HandlerOptions) *slog.Logger {
 	return slog.New(slog.NewJSONHandler(w, opts))
